d98c5e35416b0cd5203802931049cd69bda4e1f6
kevinlbt <kevin.lebot@hotmail.fr>
2024-01-15 19:42:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='test text-5xl text-red-500 m-24'>
+        hello arrose mon web
+      </div>
+      
+    </>
+  )
+}
+
+export default App
