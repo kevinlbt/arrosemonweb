9f96dc454919b10fd993af6f3c78e7ad2b94b8a6
kevinlbt <kevin.lebot@hotmail.fr>
2024-01-19 21:41:31 -0500

    commit kev, navbar, header, portfolio, contact et footer (non terminé)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
+
 export default function Navbar () {
 
     return <nav className="flex justify-between items-center">
-        <h1 className="m-12">arrose mon web</h1>
-        <ul className="m-12 flex text-red-300">
-            <li>accueill</li>
-            <li>prestations</li>
-            <li>a propos</li>
-            <li>contact</li>
-            <li>test</li>
+        <h1 className="w-2/6 lg:w-1/6 mx-24">arrose mon web</h1>
+        <ul className="m-12 flex flex-wrap">
+            <li className="hover_nav">accueil</li>
+            <li className="hover_nav">prestations</li>
+            <li className="hover_nav">a propos</li>
+            <li className="hover_nav">contact</li>
         </ul>
     </nav>
 }
