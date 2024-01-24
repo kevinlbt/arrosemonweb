7bbb76bd21e3b4b4db548c704b62da65365d37e6
kevinlbt <kevin.lebot@hotmail.fr>
2024-01-24 13:07:10 -0500

    responsive design, logo re design et divers petites modifs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Navbar ({background}) {
 
     return <nav className={`flex justify-between items-center ${background}`}>
-        <h1 className="mx-12 xl:mx-24">arrose mon web</h1>
+        <h1 className="mx-3 sm:mx-12 xl:mx-16 text-right">arrose <span>mon web</span></h1>
         <ul className="mx-7 xl:mx-12 my-12 flex flex-wrap justify-end">
             <a href="/"><li className="hover_nav">accueil</li></a>
             <a href="/#presta"><li className="hover_nav">prestations</li></a>
