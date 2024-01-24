82d769f8bc9f1a24309878dd6ae17c4af1b6eac6
Masteroyx <alexlhotellier.al@gmail.com>
2024-01-24 14:52:12 -0500

    add HashLink Footer,Header + texte + footer:hover

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,22 @@
 import React from "react";
+// import { Link } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Footer () {
 
     return <footer className="flex flex-col sm:flex-row justify-between items-center border-t-2 p-12">
-        <h1 className="w-2/6 lg:w-1/6 sm:mx-12 lg:mx-24">arrose mon web</h1>
+        <h1 className="w-2/6 lg:w-1/6 sm:mx-12 lg:mx-24"><a className ="hover_title" href="/">arrose mon web</a></h1>
         <div className="w-2/3 flex flex-col sm:flex-row justify-end items-center text-center">
             <div className="m-3 sm:m-8">
-                <h4>Social</h4>
-                <p>linkedin</p>
-                <p>github</p>
+                <p>Linkedin</p>
+                <p>Instagram</p>
+                <p>Facebook</p>
             </div>
+            
             <div className="m-3 sm:m-8">
-               <p>Get in touch</p>
-            </div>
-            <div className="m-3 sm:m-8">
-                <p>Nos prestations</p>
-                <p>A propos</p>
-                <p>Notre travail</p>
+                <p><Link to="..#presta">Nos prestations</Link></p>
+                <p><a href="/apropos">A propos</a></p>
+                <p><Link to="..#portfolio">Notre travail</Link></p>
             </div>
         </div>
     </footer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { HashLink as Link } from 'react-router-hash-link';
 
 export default function Navbar ({background}) {
 
@@ -7,7 +7,7 @@ export default function Navbar ({background}) {
         <h1 className="mx-12 xl:mx-24">arrose mon web</h1>
         <ul className="mx-7 xl:mx-12 my-12 flex flex-wrap justify-end">
             <a href="/"><li className="hover_nav">accueil</li></a>
-            <a href="/#presta"><li className="hover_nav">prestations</li></a>
+            <li className="hover_nav"><Link to="..#presta">prestations</Link></li>
             <a href="/apropos"><li className="hover_nav">a propos</li></a>
             <a href="/#contact"><li className="hover_nav">contact</li></a>
         </ul>
